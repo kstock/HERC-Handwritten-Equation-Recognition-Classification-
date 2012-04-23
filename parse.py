c3e8851febefca9867ab91b8594d49cad9ed6739
Kyle Stock <kylstock@gmail.com>
2012-04-22 17:34:52 -0700

    started parser

---
+++ parse.py
@@ -0,0 +1,63 @@
+import scipy,numpy
+from scipy import io
+
+#decoding the code!!
+#1: \\forall
+#2  \\exists
+#3  \\neg
+#4  \\left(
+#5  \\right)
+#6  x
+#7  y
+#8  z
+#9  \\neq
+#10 \\in
+#11 \\wedge
+#12 \\vee
+#13 \\rightarrow
+#14 \\leftrightarrow
+#15 \\subset
+#16 F
+#17 R
+code = [
+        '\\forall ',
+        '\\exists',
+        '\\neg',
+        '\\left(',
+        '\\right)',
+        'x',
+        'y',
+        'z',
+        '\\neq',
+        '\\in',
+        '\\wedge',
+        '\\vee',
+        '\\rightarrow',
+        '\\leftrightarrow',
+        '\\subset',
+        'F',
+        'R',
+       ]
+
+
+#open for appending, creates if doesn't already exist
+f = open('test.tex', 'a+')
+
+startDoc = '\\documentclass[a4paper,12pt]{article} \n \\begin{document}\n $'
+endDoc = '$\n \\end{document}'
+
+f.write(startDoc)
+
+
+
+#returns dict with key as matrix name, matrix as val
+labels = scipy.io.loadmat('parse.mat')
+mat = labels['parse'][0]
+
+#mat = range(1,17)
+for symbol in mat:
+  f.write(code[symbol])
+  #print code[symbol]
+
+
+f.write(endDoc)
